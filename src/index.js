293e79a0043dc20989aac14017139be29198e450
igor <vast-vast@ya.ru>
2018-10-29 11:08:53 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+import style from './style.css';
+
+class App extends Component {
+
+    render() {
+        return (
+            <div className={style.app}>
+                Hello i'am App
+            </div>
+        )
+    }
+}
+
+export default App;
